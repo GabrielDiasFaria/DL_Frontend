fcd7c08bf91140364cc571a1fe11c895d1859301
Gabriel Dias Faria <gabrieldiasfaria@gmail.com>
2020-05-14 13:51:50 +0100

    first commit

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+export default function Routes() {
+
+    const Teste = () => {
+        return (
+            <h3>Testando React 1</h3>
+        );
+    };
+
+    const Teste2 = () => {
+        return (
+            <h3>Testando React 2</h3>
+        );
+    };
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                {/* Página Inicial do Blog */}
+                <Route path="/" exact={true} component={Teste} />
+                <Route path="/teste" component={Teste2} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
