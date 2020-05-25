9aaeb4a4877d2254eb0bb16948031918a2518a3c
Gabriel Dias Faria <gabrieldiasfaria@gmail.com>
2020-05-25 17:36:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+// "homepage": "http://descomplicandolinguagens.com.br/blog",
 ## Available Scripts
 
 In the project directory, you can run:
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import BlogView from './features/BlogView/pages'
-
 export default function Routes() {
+
+    const Teste = () => {
+        return (
+            <h3>Testando React 1</h3>
+        );
+    };
+
+    const Teste2 = () => {
+        return (
+            <h3>Testando React 2</h3>
+        );
+    };
 
     return (
         <BrowserRouter>
             <Switch>
                 {/* Página Inicial do Blog */}
-                <Route path="/" exact component={BlogView} />
-
-                {/* <Route path="/teste" component={Teste2} /> */}
+                <Route path="/" exact={true} component={Teste} />
+                <Route path="/teste" component={Teste2} />
             </Switch>
         </BrowserRouter>
     )
